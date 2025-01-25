e12abb48b797bc28fcc483bfc141c75bafe9e1b1
dali-333 <abdellatifmouhamedali@gmail.com>
2025-01-25 14:02:25 +0100

    exp trial

--- components/exp_component.gd
+++ components/exp_component.gd
@@ -3,8 +3,17 @@ extends Node
 
 @export var max_level : int = 10
 @export var experience_to_level : float = 100;
-@export var current_level : int = 1;
-@export var current_experience : int = 0;
+@export var current_level : int = 1:
+	set(value):
+		current_level = value
+		level_up.emit()
+@export var current_experience : int = 0:
+	set(value):
+		current_experience = value
+		exp_gained.emit()
+
+signal level_up(new_level : int)
+signal exp_gained(new_exp : float)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
