eb6106595015886a4cea52881a7b64c805797c7f
dali-333 <abdellatifmouhamedali@gmail.com>
2025-01-26 02:54:39 +0100

    stuff

--- components/die_component.gd
+++ components/die_component.gd
@@ -3,9 +3,17 @@ extends Node
 
 @export var actor : Node3D
 @export var stats : StatsComponent
+@onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready() -> void:
 	stats.no_health.connect(die)
 
 func die() -> void:
+	for child1 in player.get_children():
+		if child1.name == "Player":
+			for child2 in child1.get_children():
+				if child2 is ExperienceComponent:
+					print("xd")
+					var player_exp_component = child2
+					player_exp_component.add_experience(stats.xp_gain)
 	actor.queue_free()
---
+++ player/ui.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var health_bar: ProgressBar = $VBoxContainer/HealthBar
+@onready var xp_bar: ProgressBar = $VBoxContainer/XPBar
+@onready var shield_cooldown_label: Label = $VBoxContainer/ShieldCooldownLabel
+@onready var level_label: Label = $VBoxContainer/LevelLabel
+@onready var dash_cooldown_label: Label = $VBoxContainer/DashCooldownLabel
+@onready var player : CharacterBody3D = self.get_parent()
+
+@export var stats : StatsComponent 
+@export var exp : ExperienceComponent
+@export var attack : AttackInputComponent
+@export var move : MoveInputComponent
+
+
+func _ready() -> void:
+	exp.level_up.connect(
+		func():
+			xp_bar.max_value = exp.experience_to_level
+			xp_bar.min_value = exp.experience_to_level - 100
+	)
+	health_bar.value = stats.health
+	xp_bar.value = exp.current_experience
+	level_label.text = "Level : " + str(exp.current_level)
+	shield_cooldown_label.text = str(round(attack.shield_cooldown_timer))
+	dash_cooldown_label.text = str(round(move.dash_cooldown_timer))
+	
+
+func _process(delta: float) -> void:
+	health_bar.value = stats.health
+	xp_bar.value = exp.current_experience
+	level_label.text = "Level : " + str(exp.current_level)
+	shield_cooldown_label.text = str(round(attack.shield_cooldown_timer))
+	dash_cooldown_label.text = str(round(move.dash_cooldown_timer))
